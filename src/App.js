aa6057ac39738f439ec6e1fb7b256b10d0d0ed27
Darren Tebo <darrentebo83@outlook.com>
2020-07-23 19:21:34 -0400

    added ADD_FEATURE action and action creator

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
+import { addFeature } from './actions/actions';
+
 const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    additionalFeatures: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
+  // const state = {
+  //   additionalPrice: 0,
+  //   car: {
+  //     price: 26395,
+  //     name: '2019 Ford Mustang',
+  //     image:
+  //       'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+  //     features: []
+  //   },
+  //   additionalFeatures: [
+  //     { id: 1, name: 'V-6 engine', price: 1500 },
+  //     { id: 2, name: 'Racing detail package', price: 1500 },
+  //     { id: 3, name: 'Premium sound system', price: 500 },
+  //     { id: 4, name: 'Rear spoiler', price: 250 }
+  //   ]
+  // };
+
+  const mapStateToProps = state => {
+    return {
+      car: state.car,
+      additionalFeatures: state.additionalFeatures,
+      additionalPrice: state.additionalPrice
+    }
   };
 
+  const addFeature = (feature) => {
+    
+  };
+  
   return (
     <div className="boxes">
       <div className="box">
@@ -37,4 +51,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default connect(mapStateToProps, { addFeature })(App);
