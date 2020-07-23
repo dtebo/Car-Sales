054a49cbe595376cc8355d4668de306a716c576d
Darren Tebo <darrentebo83@outlook.com>
2020-07-23 19:05:33 -0400

    added reducer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
+
+import reducer from './reducers/reducer';
+
+const store = createStore(reducer);
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
